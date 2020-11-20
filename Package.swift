3ac69b4dec9c720bb2372f9a86189680dbd5dcb1
Peder Toftegaard Olsen <pdr@theperfectapp.com>
2020-11-20 15:18:28 +0100

    Initial commit.

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "TPA-Apple-SDK",
+    products: [
+        .library(
+            name: "ThePerfectApp",
+            targets: ["ThePerfectApp"]
+        ),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ThePerfectApp",
+            url: "https://sdk.tpa.io/iOS/6.0.0/ThePerfectApp-6.0.0.zip",
+            checksum: "1fd0dbb3fb7d8f9b404f3aac8af78189cccff5b259e0839b61d5e0ace34dae91"
+        ),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,6 @@
+# TPA-Apple-SDK
+
+Package Repository for TPA's iOS SDKs.
+
+
+Learn more at https://theperfectapp.com/.
